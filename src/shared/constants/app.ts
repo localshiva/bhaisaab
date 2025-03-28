c7af5e7a90da4f61fc1fc6ea7b8d329382db62bf
Rahul Shetty <shettyrahul8june@gmail.com>
2025-03-28 12:19:02 +0530

    feat(config): update typography by adding new fonts

---
+++ src/shared/constants/app.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = "Bhaisaab";
+export const APP_DESCRIPTION =
+  "Bhaisaab (Bhai + Hisaab) is a web application to track family expense and gather information about the amount available each month";
