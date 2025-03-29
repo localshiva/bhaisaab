bed74d30aa1a06a6594f8d37a940b847400b0dac
Rahul Shetty <admin@rahulshetty.dev>
2025-03-30 02:16:06 +0530

    refactor(ui): enhance AppSidebar component to accept user prop

--- src/shared/components/app/app-sidebar/app-sidebar.tsx
+++ src/shared/components/app/app-sidebar/app-sidebar.tsx
@@ -2,10 +2,16 @@
 
 import { AppSidebarHeader } from "@bhaisaab/shared/components/app/app-sidebar/app-sidebar-header";
 import { Sidebar } from "@bhaisaab/shared/components/core/sidebar";
+import { User } from "next-auth";
 
 import SidebarNav from "./sidebar-nav";
 
-export const AppSidebar = () => {
+interface AppSidebarProps {
+  user?: User;
+}
+
+export const AppSidebar = ({ user }: AppSidebarProps) => {
+  console.info(user);
   return (
     <Sidebar collapsible="icon">
       <AppSidebarHeader />
