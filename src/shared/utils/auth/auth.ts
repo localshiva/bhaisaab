977d872283ef0061306f402ea91521ef42dc1d7e
Rahul Shetty <admin@rahulshetty.dev>
2025-03-30 14:26:28 +0530

    refactor(auth): update Google provider config with additional scopes

--- src/shared/utils/auth/auth.ts
+++ src/shared/utils/auth/auth.ts
@@ -7,7 +7,19 @@ import { serverEnv } from "../env-vars/server.env";
 export const authDetails = Google({});
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      authorization: {
+        params: {
+          scope:
+            "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive",
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
   callbacks: {
     signIn({ user }) {
       const allowedEmails = serverEnv.ALLOWED_EMAILS;
